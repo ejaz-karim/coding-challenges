ac18e126c108d5ea00595e5e5f36d92184e22381
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-04-19 01:41:31 +0100

    Update 322-coin-change.go

--- leetcode/322-coin-change.go
+++ leetcode/322-coin-change.go
@@ -7,6 +7,22 @@ func coinChange(coins []int, amount int) int {
 		return 0
 	}
 
+	var highestCoin int
+	for index, coin := range coins{
+
+		if index == 0{
+			highestCoin = coin
+			continue
+		}
+
+		if coin > highestCoin {
+			highestCoin = coin
+		}
+		
+	}
+
+	fmt.Print(highestCoin)
+
 	return -1
 
 }
@@ -14,7 +30,7 @@ func coinChange(coins []int, amount int) int {
 func main() {
 	fmt.Println("test")
 
-	var amount int = 1
+	// var amount int = 1
 
 	coinChange([]int{1, 2, 5}, 11)
 }
