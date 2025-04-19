df4a6ce71ccbfd4f1b62cdc81a4d049328b53340
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-04-19 02:42:31 +0100

    Update 322-coin-change.go

--- leetcode/322-coin-change.go
+++ leetcode/322-coin-change.go
@@ -1,36 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func coinChange(coins []int, amount int) int {
 	if amount == 0 {
 		return 0
 	}
 
-	var highestCoin int
-	for index, coin := range coins{
 
-		if index == 0{
-			highestCoin = coin
-			continue
+
+	sort.Ints(coins)
+
+
+
+	buffer := amount
+	count := 0
+	for buffer > 0 {
+
+		if len(coins)-1 <= buffer{
+			buffer -= (len(coins)-1)
+			count++
+			
+
+		}else if len(coins)-1 > buffer {
+			coins = coins[:(len(coins)-1)]
+
+			count++
 		}
 
-		if coin > highestCoin {
-			highestCoin = coin
-		}
-		
 	}
 
-	fmt.Print(highestCoin)
+
 
 	return -1
 
 }
 
 func main() {
-	fmt.Println("test")
-
-	// var amount int = 1
-
 	coinChange([]int{1, 2, 5}, 11)
+	coinChange([]int{2}, 3)
+	coinChange([]int{1}, 0)
 }
