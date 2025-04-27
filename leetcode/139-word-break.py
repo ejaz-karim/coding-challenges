efe9de1c1002e7eaa6cc44648a8f751364847438
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-04-27 19:39:20 +0100

    Update 139-word-break.py

--- leetcode/139-word-break.py
+++ leetcode/139-word-break.py
@@ -1,5 +1,19 @@
 
+from typing import List
+
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+       pass
         
+
+
+
+def main():
+   print(   Solution().wordBreak(     "leetcode", ["leet","code"]))
+   print("test")
+
+
+if __name__ == '__main__':
+  main()
+  
