b428d49be5c4a6baf727a9be062a77729f908c00
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-05-09 05:49:24 +0100

    Update 139-word-break.py

--- leetcode/139-word-break.py
+++ leetcode/139-word-break.py
@@ -1,19 +1,22 @@
-
+import sys
 from typing import List
 
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-       pass
-        
+        if len(s) <= 1 or len(s) <= 300:
+            print("string 's' is out of constraint")
+            sys.exit(1)
+        elif len(wordDict) <= 1 or len(wordDict) <= 1000:
+            print("wordDict is out of constraint")
+            sys.exit(1)
+    
 
 
 
 def main():
-   print(   Solution().wordBreak(     "leetcode", ["leet","code"]))
-   print("test")
+    print(Solution().wordBreak("leetcode", ["leet", "code"]))
 
 
-if __name__ == '__main__':
-  main()
-  
+if __name__ == "__main__":
+    main()
