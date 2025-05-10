4c3a36a4c1bf0f4a2f9531e0dc8e486312ae8be8
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2025-05-10 06:22:21 +0100

    Update 139-word-break.py

--- leetcode/139-word-break.py
+++ leetcode/139-word-break.py
@@ -6,10 +6,17 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         if len(s) <= 1 or len(s) <= 300:
             print("string 's' is out of constraint")
-            sys.exit(1)
+            return False
         elif len(wordDict) <= 1 or len(wordDict) <= 1000:
             print("wordDict is out of constraint")
-            sys.exit(1)
+            return False
+        elif len(wordDict[str]) <= 1 or len(wordDict[str]) <= 20:
+            print("wordDict[i] is out of constraint")
+            return False
+        elif not s.islower().isalpha() or not wordDict[str].islower().isalpha():
+            print("s, or wordDict is either not lowercase or not alphabetic")
+            return False
+        # elif wordDict.count()
     
 
 
